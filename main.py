6a563614c166712539d14d1fbb5bc6e4d098cbd6
Toms <tomsterauds@gmail.com>
2023-07-27 22:13:12 +0300

    impemented basic audio recording from mic, print it in transcript

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import os
 import openai
+import sounddevice as sd
+import numpy as np
+from dotenv import load_dotenv
+from scipy.io import wavfile
 
-# Read the API key from the .env file
-with open(".env", "r") as f:
-    for line in f:
-        if line.startswith("OPENAI_API_KEY"):
-            api_key = line.split("=")[1].strip()
+# Load environment variables from .env file
+load_dotenv()
+
+# Function to record audio
+def record_audio(file_path, duration=5):
+    print(f"Recording audio for {duration} seconds... (Please speak)")
+    audio_data = sd.rec(int(duration * 44100), samplerate=44100, channels=1, dtype=np.int16)
+    sd.wait()
+    wav_data = np.array(audio_data, dtype=np.int16)
+    wavfile.write(file_path, 44100, wav_data)
+
+# Function to get API key from string or environment variable
+def get_api_key(api_key_string=None):
+    if api_key_string is None:
+        # Get the API key from environment variable if None provided
+        api_key = os.getenv("OPENAI_API_KEY")
+        if api_key is None:
+            print("Error: API key not found in environment variable (OPENAI_API_KEY).")
+            exit()
+    else:
+        api_key = api_key_string
+
+    return api_key
 
 # Set the API key
-openai.api_key = api_key
+api_key_string = None  # Replace this string with your actual API key or leave it as None
+openai.api_key = get_api_key(api_key_string)
 
-# Path to the audio file
-audio_file_path = "/home/toms/code/audio-to-text/demo.mp4"
+
+# Prompt the user to record audio
+audio_file_path = "/home/toms/code/audio-to-text/temp_audio.wav"
+record_audio(audio_file_path)
 
 # Ensure the audio file exists
 if not os.path.exists(audio_file_path):
@@ -21,7 +46,9 @@ if not os.path.exists(audio_file_path):
 # Open the audio file in binary read mode
 with open(audio_file_path, "rb") as audio_file:
     # Transcribe the audio using the specified engine ID
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    engine_id = "whisper-1"  # Replace this with the appropriate engine ID
+    transcript = openai.Audio.transcribe(engine_id, audio_file)
 
 # Print the transcript
+print("Transcript:")
 print(transcript)
