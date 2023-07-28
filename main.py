0d73d4aef67caa6a13b1645bfab5b662b054d90d
Toms <tomsterauds@gmail.com>
2023-07-28 14:11:58 +0300

    def main() added

--- main.py
+++ main.py
@@ -4,28 +4,33 @@ from dotenv import load_dotenv
 from utils.audio_utils import record_audio
 from utils.api_utils import get_api_key
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Set the API key
-api_key_string = None  # Replace this string with your actual API key or leave it as None
-openai.api_key = get_api_key(api_key_string)
+def main():
+    # Load environment variables from .env file
+    load_dotenv()
 
-# Prompt the user to record audio
-audio_file_path = "/home/toms/code/audio-to-text/temp_audio.wav"
-record_audio(audio_file_path)
+    # Set the API key
+    api_key_string = None  # Replace this string with your actual API key or leave it as None
+    openai.api_key = get_api_key(api_key_string)
 
-# Ensure the audio file exists
-if not os.path.exists(audio_file_path):
-    print("Error: Audio file not found.")
-    exit()
+    # Prompt the user to record audio
+    audio_file_path = "/home/toms/code/audio-to-text/temp_audio.wav"
+    record_audio(audio_file_path)
 
-# Open the audio file in binary read mode
-with open(audio_file_path, "rb") as audio_file:
-    # Transcribe the audio using the specified engine ID
-    engine_id = "whisper-1"  # Replace this with the appropriate engine ID
-    transcript = openai.Audio.transcribe(engine_id, audio_file)
+    # Ensure the audio file exists
+    if not os.path.exists(audio_file_path):
+        print("Error: Audio file not found.")
+        exit()
 
-# Print the transcript
-print("Transcript:")
-print(transcript)
+    # Open the audio file in binary read mode
+    with open(audio_file_path, "rb") as audio_file:
+        # Transcribe the audio using the specified engine ID
+        engine_id = "whisper-1"  # Replace this with the appropriate engine ID
+        transcript = openai.Audio.transcribe(engine_id, audio_file)
+
+    # Print the transcript
+    print("Transcript:")
+    print(transcript)
+
+if __name__ == "__main__":
+    main()
--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -2,7 +2,7 @@ import sounddevice as sd
 import numpy as np
 from scipy.io import wavfile
 
-def record_audio(file_path, duration=5):
+def record_audio(file_path, duration=3):
     print(f"Recording audio for {duration} seconds... (Please speak)")
     audio_data = sd.rec(int(duration * 44100), samplerate=44100, channels=1, dtype=np.int16)
     sd.wait()
